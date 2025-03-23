f1b70176b673d5f9ac196c24f8cc20244580815e
Didgeridoo_Dave <malermeister-lehnartz@gmx.de>
2025-03-23 20:33:57 +0100

    added skill-set component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
 
 @Component({
   selector: 'app-main-content',
   imports: [
     HeaderComponent,
     AboveTheFoldComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    SkillSetComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
