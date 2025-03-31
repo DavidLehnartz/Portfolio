11abe25d9776ad24b2ee8ddbcb8283465ac1bcbe
Didgeridoo_Dave <malermeister-lehnartz@gmx.de>
2025-03-31 19:36:40 +0200

    single project -join- styling

--- src/app/single-projects/project-join/project-join.component.ts
+++ src/app/single-projects/project-join/project-join.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { ProjectHeaderComponent} from "../project-header/projects-header.component";
+import { ProjectHeaderComponent } from '../project-header/projects-header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-project-join',
-  imports: [ProjectHeaderComponent],
+  imports: [ProjectHeaderComponent, RouterLink],
   templateUrl: './project-join.component.html',
-  styleUrl: './project-join.component.scss'
+  styleUrl: './project-join.component.scss',
 })
-export class ProjectJoinComponent {
-
-}
+export class ProjectJoinComponent {}
