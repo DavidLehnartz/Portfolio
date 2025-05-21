f0628577afba396f7507de757f9d2bac2d285b0a
Didgeridoo_Dave <malermeister-lehnartz@gmx.de>
2025-05-21 18:32:45 +0200

    a href binding to websites

--- src/app/coming-soon/coming-soon.component.ts
+++ src/app/coming-soon/coming-soon.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { TranslatePipe } from '@ngx-translate/core';
-import { FooterComponent } from "../main-content/contact-me/footer/footer.component";
 
 @Component({
   selector: 'app-coming-soon',
-  imports: [HeaderComponent, TranslatePipe, FooterComponent],
+  imports: [HeaderComponent, TranslatePipe],
   templateUrl: './coming-soon.component.html',
   styleUrl: './coming-soon.component.scss',
 })
